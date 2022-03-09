807ac6902f0faedf19279654cf0b286ffd92afdb
Fabien Chareun <cacrfabfr@gmail.com>
2022-03-09 17:16:34 +0100

    final touch on rps

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -7,14 +7,17 @@ const choicesArray   = ['ROCK', 'PAPER', 'SCISSORS']
 getResults = () => {
   if (computerChoice.innerText === userChoice.innerText) {
     result.innerText = "DRAW";
+    result.style.color = 'rgb(171 171 171)'
   } else if (
     (computerChoice.innerText === "SCISSORS" && userChoice.innerText === "ROCK") ||
     (computerChoice.innerText === "ROCK" && userChoice.innerText === "PAPER") ||
     (computerChoice.innerText === "PAPER" && userChoice.innerText === "SCISSORS")
   ) {
     result.innerText = "You win!";
+    result.style.color = 'rgb(0 255 114)'
   } else {
     result.innerText = "You lose!";
+    result.style.color = 'rgb(255, 0, 80)'
   }
 };
 
