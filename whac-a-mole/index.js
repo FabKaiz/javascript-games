179ccc641598308b90fb769d35958c6eea5e1014
Fabien Chareun <cacrfabfr@gmail.com>
2022-03-11 10:53:22 +0100

    whac a mole setup

---
+++ whac-a-mole/index.js
@@ -0,0 +1,7 @@
+const squares  = document.querySelectorAll('.square');
+const mole     = document.querySelector('.mole');
+const timeLeft = document.querySelector('#timeLeft');
+const score    = document.querySelector('#score');
+console.log(squares);
+console.log(mole);
+
