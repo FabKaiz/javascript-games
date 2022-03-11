4fb6f157a8040ae5d10d476854d2c39842285699
Fabien Chareun <cacrfabfr@gmail.com>
2022-03-11 12:39:40 +0100

    whac a mole done

--- whac-a-mole/index.js
+++ whac-a-mole/index.js
@@ -1,7 +1,6 @@
 const squares  = document.querySelectorAll('.square');
 const mole     = document.querySelector('.mole');
 const timeLeft = document.querySelector('#time-left');
-console.log(timeLeft);
 const score    = document.querySelector('#score');
 const startBtn = document.querySelector('#start-button');
 
@@ -9,16 +8,28 @@ let isRunning = false
 let result = 0
 let moleTimer = null
 let moleId
-let gameTimer = 3
+let gameTimer = 30
 let gameTimerId
 
 const randomSquare = () => {
   squares.forEach(square => {
     square.classList.remove('mole');
+    square.innerHTML = ''
   })
 
   const randomPosition = squares[Math.floor(Math.random() * squares.length)];
   randomPosition.classList.add('mole')
+  randomPosition.innerHTML += '<img src="./images/mole.png" />';
+
+  let img = document.getElementsByTagName('img')[0]
+  setTimeout(() => {
+    img.style.marginTop = '20px'
+    
+  }, 100);
+  setTimeout(() => {
+    img.style.marginTop = '200px'
+    
+  }, 700);
   moleId = randomPosition.id
 }
 
@@ -28,6 +39,7 @@ squares.forEach(square => {
       result++
       score.textContent = result
       square.classList.remove('mole');
+      square.innerHTML = ''
     } 
   })
 })
@@ -38,28 +50,26 @@ const moveTheMole = () => {
   moleTimer = setInterval(() => randomSquare(), 1000);
 }
 
-
-
-
-
 startBtn.addEventListener('click', () => {
-  moveTheMole()
-  gameTimerId = setInterval(() => countdown(), 1000);
-  
-  // countdown()
+  if (gameTimer == 0 && !isRunning) {
+    window.location.reload()
+  } else if (!isRunning) {
+    moveTheMole()
+    gameTimerId = setInterval(() => countdown(), 1000);
+  }
 });
 
 function countdown() {
-  if (gameTimer == 0) {
+  if (gameTimer == 0 ) {
     clearInterval(gameTimerId);
     clearInterval(moleTimer);
     alert('GAME OVER! Your score: ' + result)
     squares.forEach(square => {
       square.classList.remove('mole');
     })
-  } else {
+    isRunning = false;
+  } else  {
     gameTimer--;
-    console.log('heelkjoikghjlikjdfkjg');
     timeLeft.textContent = gameTimer;
   }
 }
