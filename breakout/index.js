9721c3314d887a9fba63dd3d16baf3a4af87474e
Fabien Chareun <cacrfabfr@gmail.com>
2022-03-15 12:26:22 +0100

    breakout

--- breakout/index.js
+++ breakout/index.js
@@ -1,14 +1,19 @@
 const gameContainer = document.querySelector('.game');
+const scoreDisplay = document.querySelector('.score')
 const blockWidth = 80;
 const blockHeight = 20;
 const borderWidth = 640;
+const borderHeight = 300;
+const ballDiameter = 10;
+let timerId
+let xDirection = 2;
+let yDirection = 2;
 
 const userStartPosition = [280, 10]
 let currentPosition = userStartPosition
 
 const ballStartPosition = [315, 40]
 let ballCurrentPosition = ballStartPosition
-
 
 class Block {
   //Initate every corner of the block
@@ -56,8 +61,8 @@ const addBlocks = () => {
     
   }
 }
-
 addBlocks()
+
 
 // draw the user bar
 const drawUserBar = () => {
@@ -65,18 +70,19 @@ const drawUserBar = () => {
   userBar.style.bottom = currentPosition[1] + 'px'
 }
 
+
 // draw the ball
 const drawBall = () => {
   ball.style.left = ballCurrentPosition[0] + 'px'
   ball.style.bottom = ballCurrentPosition[1] + 'px'
 }
 
+
 // Add the user bar
 const userBar = document.createElement('div');
 userBar.classList.add('userBar');
 drawUserBar()
 gameContainer.appendChild(userBar);
-
 
 
 // Move the user bar
@@ -98,13 +104,73 @@ const moveUserBar = (e) => {
   }
 }
 
+
 // Add ball
 const ball = document.createElement('div');
 ball.classList.add('ball');
 drawBall()
 gameContainer.appendChild(ball);
 
+
 // Move balll
+const moveBall = () => {
+  ballCurrentPosition[0] += xDirection
+  ballCurrentPosition[1] += yDirection
+  drawBall()
+  checkCollisions()
+
+}
+
+timerId = setInterval(moveBall, 30);
+
+const changeDirection = () => {
+  
+  if (xDirection === 2 && yDirection === 2) {
+    // Go down right
+    yDirection = -2
+    return
+  }
+
+  if (xDirection === 2 && yDirection === -2) {
+    // Go left down
+    xDirection = -2
+    return
+  }
+
+  if (xDirection === -2 && yDirection === -2) {
+    // Go up left
+    yDirection = 2
+    return
+  }
+
+  if (xDirection === -2 && yDirection === 2) {
+    // Go down left
+    xDirection = 2
+    return
+  }
+
+}
+
+// Check for collisions
+const checkCollisions = () => {
+  // Check for wall collisions
+  // console.log(ballCurrentPosition);
+  console.log(xDirection, yDirection);
+  if (
+    ballCurrentPosition[0] >= borderWidth - ballDiameter ||
+    ballCurrentPosition[1] >= borderHeight - ballDiameter ||
+    ballCurrentPosition[0] <= 0 
+  ) {
+    changeDirection();
+  }
+
+  // Check for game over
+  if (ballCurrentPosition[1] <= 0) {
+    clearInterval(timerId)
+    scoreDisplay.innerHTML = "Game over !"
+  }
+}
+
 
 
 document.addEventListener('keydown', moveUserBar)
