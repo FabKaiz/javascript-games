7233adf8d67f001fc739ed9a4be1958f66b4520e
Fabien Chareun <cacrfabfr@gmail.com>
2022-03-15 11:31:24 +0100

    breakout

--- breakout/index.js
+++ breakout/index.js
@@ -1,6 +1,13 @@
 const gameContainer = document.querySelector('.game');
-const blockWidth = 100;
+const blockWidth = 80;
 const blockHeight = 20;
+const borderWidth = 640;
+
+const userStartPosition = [280, 10]
+let currentPosition = userStartPosition
+
+const ballStartPosition = [315, 40]
+let ballCurrentPosition = ballStartPosition
 
 
 class Block {
@@ -14,14 +21,34 @@ class Block {
 }
 
 const blocks = [
-  new Block(10, 270)
-]
+  new Block(10, 270),
+  new Block(100, 270),
+  new Block(190, 270),
+  new Block(280, 270),
+  new Block(370, 270),
+  new Block(460, 270),
+  new Block(550, 270),
 
-console.log(blocks);
+  new Block(10, 240),
+  new Block(100, 240),
+  new Block(190, 240),
+  new Block(280, 240),
+  new Block(370, 240),
+  new Block(460, 240),
+  new Block(550, 240),
+
+  new Block(10, 210),
+  new Block(100, 210),
+  new Block(190, 210),
+  new Block(280, 210),
+  new Block(370, 210),
+  new Block(460, 210),
+  new Block(550, 210),
+]
 
 const addBlocks = () => {
   for (let i = 0; i < blocks.length; i++) {
-    const block = document.createElement('block');
+    const block = document.createElement('div');
     block.classList.add('block');
     block.style.left = blocks[i].bottomLeft[0] + 'px'
     block.style.bottom = blocks[i].bottomLeft[1] + 'px'
@@ -31,3 +58,53 @@ const addBlocks = () => {
 }
 
 addBlocks()
+
+// draw the user bar
+const drawUserBar = () => {
+  userBar.style.left = currentPosition[0] + 'px'
+  userBar.style.bottom = currentPosition[1] + 'px'
+}
+
+// draw the ball
+const drawBall = () => {
+  ball.style.left = ballCurrentPosition[0] + 'px'
+  ball.style.bottom = ballCurrentPosition[1] + 'px'
+}
+
+// Add the user bar
+const userBar = document.createElement('div');
+userBar.classList.add('userBar');
+drawUserBar()
+gameContainer.appendChild(userBar);
+
+
+
+// Move the user bar
+const moveUserBar = (e) => {
+  console.log(currentPosition);
+  switch (e.key) {
+    case 'ArrowLeft':
+      if (currentPosition[0] > 0) {
+        currentPosition[0] -= 10
+        drawUserBar()
+      }
+      break;
+    case 'ArrowRight':
+      if (currentPosition[0] < borderWidth - blockWidth) {
+        currentPosition[0] += 10
+        drawUserBar()
+      }
+      break;
+  }
+}
+
+// Add ball
+const ball = document.createElement('div');
+ball.classList.add('ball');
+drawBall()
+gameContainer.appendChild(ball);
+
+// Move balll
+
+
+document.addEventListener('keydown', moveUserBar)
