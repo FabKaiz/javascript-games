4aef6724bbda8d1fe2e471efb40c6ee7c42d287f
Fabien Chareun <cacrfabfr@gmail.com>
2022-03-16 12:03:46 +0100

    css and js setup

---
+++ frogger/index.js
@@ -0,0 +1,4 @@
+const timeLeftDisplay   = document.getElementById('time-left');
+const resultLeftDisplay = document.getElementById('result');
+const startBtn          = document.getElementById('start-button');
+const squares           = document.querySelectorAll('.grid div');
