f41c35ff69f01645158f97409fc6225792d56c40
Fabien Chareun <cacrfabfr@gmail.com>
2022-03-18 10:30:35 +0100

    connect four

---
+++ connect-four/index.js
@@ -0,0 +1,4 @@
+const timeLeftDisplay   = document.getElementById('time-left');
+const resultDisplay     = document.getElementById('result');
+const startBtn          = document.getElementById('start-button');
+
