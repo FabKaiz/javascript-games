74d7efe2ee3db4e4328c7a4cb2ac4a6c8ce9af00
Fabien Chareun <cacrfabfr@gmail.com>
2022-03-25 15:48:22 +0100

    handle movement done need to add logic

--- 2048/index.js
+++ 2048/index.js
@@ -12,14 +12,13 @@ const grid = new Grid(gameContainer)
 grid.randomEmptyCell().tile = new Tile(gameContainer)
 grid.randomEmptyCell().tile = new Tile(gameContainer)
 setupInput()
-console.log(grid.cellsByColumn);
+
 
 function setupInput() {
   window.addEventListener('keydown', handleInput, { once: true });
 }
 
 function handleInput(e) {
-  console.log(e.key);
   switch (e.key) {
     case 'ArrowUp':
       moveUp()
@@ -37,6 +36,8 @@ function handleInput(e) {
       setupInput()
       return;
   }
+  // TODO: add game logic
+  
   setupInput()
 }
 
@@ -45,18 +46,27 @@ function moveUp() {
 }
 
 function moveDown() {
+  return slideTiles(grid.cellsByColumn.map(column => [...column].reverse()))
+}
 
+function moveLeft() {
+  return slideTiles(grid.cellsByRow)
+}
+
+function moveRight() {
+  return slideTiles(grid.cellsByRow.map(row => [...row].reverse()))
 }
 
 function slideTiles(cells) {
   cells.forEach(group => {
     for (let i = 1; i < group.length; i++) {
       const cell = group[i]
+      if (cell.tile == null) continue
       let lastValidCell
       for (let j = i - 1; j >= 0; j--) {
-        const moveToCell = group[j];
-        if (!moveToCell.canAccept(cell.tile)) break;
-        lastValidCell = moveToCell;
+        const moveToCell = group[j]
+        if (!moveToCell.canAccept(cell.tile)) break
+        lastValidCell = moveToCell
       }
 
       if (lastValidCell != null) {
@@ -66,7 +76,6 @@ function slideTiles(cells) {
           lastValidCell.tile = cell.tile;
         }
         cell.tile = null;
-        
       }
     }
   })
--- 2048/javascript/Grid.js
+++ 2048/javascript/Grid.js
@@ -10,21 +10,33 @@ export default class Grid {
     gridElement.style.setProperty('--grid-size', GRID_SIZE);
     gridElement.style.setProperty('--cell-size', `${CELL_SIZE}vmin`);
     gridElement.style.setProperty('--cell-gap', `${CELL_GAP}vmin`);
-    this.#cells = createCellElement(gridElement).map((cellElement, index) => {
-      return new Cell(cellElement, index % GRID_SIZE, Math.floor(index / GRID_SIZE))
+    this.#cells = createCellElements(gridElement).map((cellElement, index) => {
+      return new Cell(
+        cellElement,
+        index % GRID_SIZE,
+        Math.floor(index / GRID_SIZE)
+      )
     })
   }
 
-  get cellsByColumn() {
+  get cellsByRow() {
     return this.#cells.reduce((cellGrid, cell) => {
       cellGrid[cell.x] = cellGrid[cell.x] || []
       cellGrid[cell.x][cell.y] = cell
       return cellGrid
     }, [])
   }
+  
+  get cellsByColumn() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.x] = cellGrid[cell.x] || []
+      cellGrid[cell.y][cell.x] = cell
+      return cellGrid
+    }, [])
+  }
 
   get #emptyCells() {
-    return this.#cells.filter((cell) => cell.tile == null)
+    return this.#cells.filter(cell => cell.tile == null)
   }
 
   randomEmptyCell() {
@@ -34,10 +46,11 @@ export default class Grid {
 }
 
 class Cell {
-  #cellElement
-  #x
-  #y
-  #tile
+  #cellElement;
+  #x;
+  #y;
+  #tile;
+  #mergeTile;
 
   constructor(cellElement, x, y) {
     this.#cellElement = cellElement;
@@ -45,29 +58,51 @@ class Cell {
     this.#y = y;
   }
 
-  get x() { return this.#x; }
+  get x() {
+    return this.#x;
+  }
 
-  get y() { return this.#y; }
-  
+  get y() {
+    return this.#y;
+  }
+
   get tile() {
-    return this.#tile
+    return this.#tile;
   }
 
   set tile(value) {
-    this.#tile = value
+    this.#tile = value;
     if (value == null) return;
-    this.#tile.x = this.#x
-    this.#tile.y = this.#y
+    this.#tile.x = this.#x;
+    this.#tile.y = this.#y;
+  }
+
+  get mergeTile() {
+    return this.#mergeTile;
+  }
+
+  set mergeTile(value) {
+    this.#mergeTile = value;
+    if (value == null) return;
+    this.#mergeTile.x = this.#x;
+    this.#mergeTile.y = this.#y;
+  }
+
+  canAccept(tile) {
+    return (
+      this.tile == null ||
+      (this.mergeTile == null && this.tile.value === tile.value)
+    )
   }
 }
 
-const createCellElement = (gridElement) => {
+function createCellElements(gridElement) {
   const cells = []
-  for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+  for (let i = 0; i < (GRID_SIZE * GRID_SIZE); i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
     cells.push(cell);
-    gridElement.appendChild(cell);
+    gridElement.append(cell);
   }
   return cells
 }
