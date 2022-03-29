8a5720405cf0811d27148561dcc21e0fa6e276b9
Fabien Chareun <cacrfabfr@gmail.com>
2022-03-29 11:29:49 +0200

    keyboard

--- Wordle/index.js
+++ Wordle/index.js
@@ -1,3 +1,47 @@
-const gameContainer = document.querySelector('.game');
-const gameMsg       = document.getElementById('game-msg');
+const gameContainer     = document.querySelector('.game');
+const keyboardContainer = document.querySelector('.keyboard');
+
+const keys = [
+  'Q',
+  'W',
+  'E',
+  'R',
+  'T',
+  'Y',
+  'U',
+  'I',
+  'O',
+  'P',
+  'A',
+  'S',
+  'D',
+  'F',
+  'G',
+  'H',
+  'J',
+  'K',
+  'L',
+  'ENTER',
+  'Z',
+  'X',
+  'C',
+  'V',
+  'B',
+  'N',
+  'M',
+  '⌫',
+]
+
+
+const handleClick = () => {
+  console.log('clicked');
+}
+
+keys.forEach(key => {
+  const buttonElement = document.createElement('button');
+  buttonElement.textContent = key 
+  buttonElement.setAttribute('id', key)
+  buttonElement.addEventListener('click', handleClick)
+  keyboardContainer.append(buttonElement)
+})
 
